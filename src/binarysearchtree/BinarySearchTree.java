f64e313828e07936819808fb9dc74b7caf6517d6
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-06-23 21:37:14 +0800

    Add loop to menu

--- src/binarysearchtree/BinarySearchTree.java
+++ src/binarysearchtree/BinarySearchTree.java
@@ -3,8 +3,10 @@ package binarysearchtree;
 public class BinarySearchTree {
     public static void main(String[] args) {
         BSTMenu bstMenu = new BSTMenu();
-        bstMenu.printMainMenu();
-        int selectedOperation = bstMenu.getIntInput();
-        bstMenu.performOperation(selectedOperation);
+        do {
+            bstMenu.printMainMenu();
+            int selectedOperation = bstMenu.getIntInput();
+            bstMenu.performOperation(selectedOperation);
+        } while(true);
     }
 }
