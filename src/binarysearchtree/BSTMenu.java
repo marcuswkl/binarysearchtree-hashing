76cd697b556f93c0dd51e7162e8db2d64c33f35f
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-06-23 20:56:22 +0800

    Add BSTMenu class and implement printMainMenu()

---
+++ src/binarysearchtree/BSTMenu.java
@@ -0,0 +1,12 @@
+package binarysearchtree;
+
+public class BSTMenu {
+    public void printMainMenu() {
+        System.out.println("Welcome to Binary Search Tree!");
+        System.out.println("1. Insertion");
+        System.out.println("2. Deletion");
+        System.out.println("3. Traversal");
+        System.out.println("0. Exit");
+        System.out.println("Please input an integer to select your desired operation.");
+    }
+}
--- src/binarysearchtree/BinarySearchTree.java
+++ src/binarysearchtree/BinarySearchTree.java
@@ -1,9 +1,8 @@
 package binarysearchtree;
 
 public class BinarySearchTree {
-
     public static void main(String[] args) {
-        System.out.println("Binary Search Tree");
+        BSTMenu bstMenu = new BSTMenu();
+        bstMenu.printMainMenu();
     }
-
 }
