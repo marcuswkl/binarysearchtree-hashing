44f9f485b53697caffca8cfaddad2aebf6064f70
Amber1004 <ambersyy@gmail.com>
2021-06-26 16:05:26 +0800

    Add printTraversalMenu method

--- src/binarysearchtree/BSTTraversal.java
+++ src/binarysearchtree/BSTTraversal.java
@@ -1,7 +1,12 @@
 package binarysearchtree;
 
 public class BSTTraversal {
+    public void printTraversalMenu(){
+        System.out.println("Select traversal option:");
+        System.out.println("1. Pre-order traversal");
+        System.out.println("2. In-order traversal");
+        System.out.println("3. Post-order traversal");
+    }
 
 
-   
 }
