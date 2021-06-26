8c7abb25ac98714e0e2819cedb4b542a6b239ee1
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-06-26 17:19:18 +0800

    Add printInsertMenu() to display instructions

--- src/binarysearchtree/BSTInsertion.java
+++ src/binarysearchtree/BSTInsertion.java
@@ -2,4 +2,9 @@ package binarysearchtree;
 
 public class BSTInsertion {
 
+    public void printInsertMenu() {
+        System.out.println("Please input a list of integers separated by commas with a space.");
+        System.out.println("For example: 1, 2, 3, 4, 5");
+    }
+
 }
