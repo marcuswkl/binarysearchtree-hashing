a23af44d76b135247db0ba75e23a749f3ed0cbaa
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-06-26 17:24:07 +0800

    Add getInsertionInput() to obtain user input
    Convert user input into string array then into int array

--- src/binarysearchtree/BSTInsertion.java
+++ src/binarysearchtree/BSTInsertion.java
@@ -1,10 +1,23 @@
 package binarysearchtree;
 
+import java.util.Scanner;
+
 public class BSTInsertion {
 
     public void printInsertMenu() {
         System.out.println("Please input a list of integers separated by commas with a space.");
         System.out.println("For example: 1, 2, 3, 4, 5");
+    }
+
+    public int[] getInsertionInput() {
+        Scanner keyboardInput = new Scanner(System.in);
+        String stringInput = keyboardInput.nextLine();
+        String[] stringArray = stringInput.split(", ");
+        int[] intArray = new int[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+            intArray[i] = Integer.parseInt(stringArray[i]);
+        }
+        return intArray;
     }
 
 }
